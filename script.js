f3331d7b5719888a4d2821cdfb714dd55ef9528a
A__Djordjevich <acadjordjevic02@gmail.com>
2025-02-03 16:45:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ function GoOnElement(adresa){
     var link = document.getElementById(adresa);
     link.scrollIntoView();
 }
-
-function downloadDocument(url, filename) {
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
